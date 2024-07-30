eaa84dc190c30b95f3d233b4cdd6087efb3bb7c3
Filipe Rodrigues <73905894+filipe-rds@users.noreply.github.com>
2024-07-30 11:12:36 -0300

    Atualizando detalhes de bibliotecas ✔

--- Consumir_Dados_MLflow.py
+++ Consumir_Dados_MLflow.py
@@ -1,5 +1,8 @@
 import os
 
+# Instalando biblioteca do MLflow
+os.system('pip install mlflow --quiet')
+
 # Exibir a URL do MLflow UI
 print("\n\nMLflow UI está rodando em http://localhost:5000\n")
 print("Caso queria cancelar a execução do MLflow, pressione Ctrl + C no terminal.\n\n\n\n")
